b288ad34b67930de633cd93c48b71eea5e8e3aa5
SenseiFavour <fchukwuedo@gmail.com>
2019-02-26 16:10:27 +0100

    Update google-sheets-html.js

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -15,7 +15,8 @@ google.load('visualization', '1', {
 var visualization;
 
 function drawVisualization() {
-    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/13Mb-Rerw0716AgAljf5vg7EKFRB1fUV9233SD_KoKn0/edit?usp=sharing');
+    //var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/13Mb-Rerw0716AgAljf5vg7EKFRB1fUV9233SD_KoKn0/edit?usp=sharing');
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1ZAVnmfwwenQ9-2Sgr-UQ6HW1WjCpoILL2GQCMXthL6Y/edit#gid=0');
     query.setQuery('SELECT A, B, C, D, E, F, G, H label A "S/N", B "STATES", C "APC", D "PDP",E "DIFFERENCE",F "WHO WON",G "% APC",H "% PDP"');
     query.send(handleQueryResponse);
 }
